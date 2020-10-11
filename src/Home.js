f027dda41b555136a39be1ae201665589c0cfbe2
Murad Cholukov <muradusa@gmail.com>
2020-10-11 11:45:57 -0700

    adding channelnames to database

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,70 @@
-import { Button } from "@material-ui/core";
-import React from "react";
-import { useSelector } from "react-redux";
-import { auth } from "./firebase";
+import { Button, Avatar } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import db, { auth } from "./firebase";
 import { selectUser } from "./features/userSlice";
 import "./Home.css";
+import { Add } from "@material-ui/icons";
+import setChannelInfo from "./features/appSlice";
 
 function Home() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  const [channels, setChannels] = useState([]);
+
+  useEffect(() => {
+    db.collection("channels").onSnapshot((snapshot) =>
+      setChannels(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          channel: doc.data(),
+        }))
+      )
+    );
+  }, []);
+
+  const handleAddChannel = () => {
+    const channelName = prompt("add channel name");
+    if (channelName) {
+      db.collection("channels").add({
+        channelName: channelName,
+      });
+    }
+  };
+
   return (
     <div className="home">
-      {console.log(user)}
-      <h1>This is Home Page</h1>
-      <Button onClick={() => auth.signOut()}>Logout</Button>
-      <img src={user.photo} alt="" />
+      <div className="home__header">
+        {console.log(user)}
+        <h1>This is Home Page</h1>
+        <Button onClick={() => auth.signOut()}>Logout</Button>
+        <Avatar src={user.photo} />
+        <h3>{user.displayName}</h3>
+      </div>
+      <div className="home__main">
+        <div className="sidebar">
+          <h1>Sidebar</h1>
+          <Add onClick={handleAddChannel} />
+          {channels.map(({ id, channel }) => (
+            <div
+              // onClick={() =>
+              //   dispatch(
+              //     setChannelInfo({
+              //       channelId: id,
+              //       channelName: channel.channelName,
+              //     })
+              //   )
+              // }
+              className="channel"
+            >
+              <p>{channel.channelName}</p>
+            </div>
+          ))}
+        </div>
+        <div className="chat">
+          <h1>chat</h1>
+        </div>
+      </div>
     </div>
   );
 }
