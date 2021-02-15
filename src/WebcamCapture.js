d39a390e02ca699896b03ae51f55a6a284b2572d
Murad Cholukov <muradusa@gmail.com>
2021-02-15 11:05:58 -0800

    adding webcam

--- src/WebcamCapture.js
+++ src/WebcamCapture.js
@@ -1,7 +1,8 @@
-import React, { useRef, useCallback } from "react";
+import React, { useRef, useCallback, useState } from "react";
 import Webcam from "react-webcam";
 
 const WebcamCapture = () => {
+  const [imgSrc, setImgSrc] = useState(null);
   const videoConstraints = {
     width: 1280,
     height: 720,
@@ -11,19 +12,23 @@ const WebcamCapture = () => {
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
-  }, [webcamRef]);
+    setImgSrc(imageSrc);
+    console.log(imageSrc);
+    console.log(imageSrc[0]);
+  }, [webcamRef, setImgSrc]);
 
   return (
     <>
-      {/* <Webcam
+      <Webcam
         audio={false}
         height={200}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
         width={300}
         videoConstraints={videoConstraints}
-      /> */}
+      />
       <button onClick={capture}>Capture photo</button>
+      {imgSrc && <img src={imgSrc} />}
     </>
   );
 };
